707fc2e7dbf0d9e6290f2fac70ddffc8cff53ffe
Gabriel Bernardes Tacconi Lourenço <gabrieltacconi147@gmail.com>
2019-06-17 14:26:15 -0300

    tempo de jogo

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TEMPO_DE_JOGO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int horaInicial, horaFinal, duracao;
+
+            Console.WriteLine("DIGITE O HORARIO DE INICIO DO JOGO");
+            horaInicial = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("DIGITE A HORA FINAL DO JOGO");
+            horaFinal = int.Parse(Console.ReadLine());
+
+            if (horaInicial < horaFinal)
+            {
+                duracao = horaInicial - horaFinal;
+            }
+            else
+            {
+                duracao = 24 - horaInicial + horaFinal;
+            }
+
+            Console.WriteLine("O JOGO DUROU " + duracao + " HORAS ");
+            Console.ReadLine();
+
+        }
+    }
+}
